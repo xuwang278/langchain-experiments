0ac7d75d5d5bbdebf653449ff1ab8bd17084bc37
xuwang278 <xuwang278@gmail.com>
2024-07-21 15:58:47 -0500

    updated RAG_Ollama

--- RAG_Ollama/main.py
+++ RAG_Ollama/main.py
@@ -5,15 +5,12 @@ from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import PromptTemplate
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
-
-# from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain.vectorstores.utils import filter_complex_metadata
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
-
-# Create the prompte from the template.
+# Create the prompt from the template.
 promptTemplate = """Answer the question as precise as possible using the provided context. If the answer is
     not contained in the context, say "answer not available in context" \n\n
     Context: {context}
@@ -25,15 +22,25 @@ modelSel = ""
 
 
 # Load the PDF file to ChromaDB
+import time
+
+
 def loadDataFromPDFFile(filePath):
+    print("Loading PDF...")
     loader = PyPDFLoader(filePath)
     pages = loader.load_and_split()
+    print(f"Number of pages: {len(pages)}")
     chunks = filter_complex_metadata(pages)
+    print(f"Number of chunks: {len(chunks)}")
 
-    # Use Ollama's Embedding
+    for doc in chunks[:5]:  # Print vectors for the first 5 documents
+        print(doc)  # Ensure embeddings are not all zero or meaningless
+
     embedding_model = OllamaEmbeddings()
-
+    print("Creating vector store...")
+    start_time = time.time()
     vector_store = Chroma.from_documents(documents=chunks, embedding=embedding_model)
+    print(f"Vector store created in {time.time() - start_time:.2f} seconds.")
     return vector_store
 
 
@@ -45,11 +52,12 @@ def modelResponse(message, history):
     )
 
     # Initiate the retriever
-    dbLoaded = loadDataFromPDFFile("./c2n.pdf")
+    dbLoaded = loadDataFromPDFFile("./c2n_short2.pdf")
     retriever = dbLoaded.as_retriever(
         search_type="similarity_score_threshold",
-        search_kwargs={"k": 5, "score_threshold": 0.2},
+        search_kwargs={"k": 2, "score_threshold": 0.001},
     )
+
     hpChain = (
         {"context": retriever, "question": RunnablePassthrough()}
         | prompt
@@ -60,7 +68,6 @@ def modelResponse(message, history):
 
 
 if __name__ == "__main__":
-
     # read configuration file
     conf = {}
     with open("config.json", "r") as confFile:
